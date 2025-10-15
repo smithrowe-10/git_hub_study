7e052029f6abc8909b4ecb6c69ffb2b53e14e2d8
smithrowe-10 <smj0418smj@gmail.com>
2025-10-15 11:29:36 +0900

    test.java 파일수정

--- src/test.java
+++ src/test.java
@@ -1,5 +1,8 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.printlns("test 출력");
+		System.out.printlns("test 출력");
+		System.out.printlns("test 출력");
+		System.out.printlns("test 출력");
 	}
 }
