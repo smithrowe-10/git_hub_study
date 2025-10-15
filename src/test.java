c4d85cf47e0dc034a4bcf06342fa629e7fb43794
smithrowe-10 <smj0418smj@gmail.com>
2025-10-15 11:36:42 +0900

    test.java 파일 수정3

--- src/test.java
+++ src/test.java
@@ -1,8 +1,5 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.printlns("test 출력");
-		System.out.printlns("test 출력");
-		System.out.printlns("test 출력");
-		System.out.printlns("test 출력");
 	}
 }
